02beefda34d74c080489f5cb89d836efff21b99c
rwietter <mauriciobw17@gmail.com>
2020-11-01 20:40:30 -0300

    add: landing page

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ const withImages = require('next-images');
 const withFonts = require('next-fonts');
 
 module.exports = withImages(
-  withFonts({
-    images: {
-      deviceSizes: [320, 420, 768, 1024, 1200],
-      iconSizes: [],
-      domains: [],
-      path: '/public/background/',
-      loader: 'default',
-    },
-  })
+  withFonts()
 );
